89f579517e863acd55dee5f2eebd021b6831a3ad
LiangXia <liangxia@live.com>
2023-06-18 13:58:11 +0800

    update

--- examples/kayura-skeleton/kayura-skeleton-webapi/src/main/java/org/kayura/skeleton/rest/BlankWebApi.java
+++ examples/kayura-skeleton/kayura-skeleton-webapi/src/main/java/org/kayura/skeleton/rest/BlankWebApi.java
@@ -43,53 +43,88 @@ public class BlankWebApi {
     this.blankService = blankService;
   }
 
+  /**
+   * 查询空白页面
+   *
+   * @param query      空白查询条件
+   * @param pageClause 分页条件
+   * @return 空白页面列表
+   */
   @PostMapping("/blank/page")
   @Secured(resource = SKE_BLANK, actions = QUERY)
-  public HttpResult selectBlankPage(@RequestBody BlankQuery query,
-                                    PageClause pageClause) {
+  public HttpResult selectBlankPage(@RequestBody BlankQuery query, PageClause pageClause) {
     PageList<BlankVo> pageList = blankService.selectBlankPage(query, pageClause);
     return HttpResult.okBody(pageList);
   }
 
+  /**
+   * 查询空白列表
+   *
+   * @param query         空白查询条件
+   * @param orderByClause 排序条件
+   * @return 空白列表
+   */
   @PostMapping("/blank/query")
   @Secured(resource = SKE_BLANK, actions = QUERY)
-  public HttpResult selectBlankList(@RequestBody BlankQuery query,
-                                    OrderByClause orderByClause) {
+  public HttpResult selectBlankList(@RequestBody BlankQuery query, OrderByClause orderByClause) {
     List<BlankVo> list = blankService.selectBlankList(query, orderByClause);
     return HttpResult.okBody(list);
   }
 
+  /**
+   * 根据ID查询空白
+   *
+   * @param id 空白ID
+   * @return 空白信息
+   */
   @GetMapping("/blank/get")
   @Secured(resource = SKE_BLANK, actions = QUERY)
   public HttpResult selectById(@RequestParam("id") String id) {
-
     BlankVo model = blankService.selectById(id);
     return HttpResult.okBody(model);
   }
 
+  /**
+   * 创建空白
+   *
+   * @param payload   创建空白的数据
+   * @param loginUser 登录用户信息
+   * @return 操作结果
+   */
   @PostMapping("/blank/create")
   @Secured(resource = SKE_BLANK, actions = CREATE)
   public HttpResult createBlank(@RequestBody @Validated(Create.class) BlankPayload payload,
                                 LoginUser loginUser) {
-
     blankService.createBlank(payload, loginUser);
     return HttpResult.ok();
   }
 
+  /**
+   * 更新空白
+   *
+   * @param id        空白ID
+   * @param payload   更新空白的数据
+   * @param loginUser 登录用户信息
+   * @return 操作结果
+   */
   @PostMapping("/blank/update")
   @Secured(resource = SKE_BLANK, actions = UPDATE)
   public HttpResult updateBlank(@RequestParam("id") String id,
                                 @RequestBody @Validated(Create.class) BlankPayload payload,
                                 LoginUser loginUser) {
-
     blankService.updateBlank(id, payload, loginUser);
     return HttpResult.ok();
   }
 
+  /**
+   * 删除空白
+   *
+   * @param payload 要删除的空白ID
+   * @return 操作结果
+   */
   @PostMapping("/blank/delete")
   @Secured(resource = SKE_BLANK, actions = DELETE)
   public HttpResult deleteBlank(@RequestBody IdPayload payload) {
-
     blankService.deleteBlank(payload);
     return HttpResult.ok();
   }
