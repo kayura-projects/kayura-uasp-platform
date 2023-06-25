4f94c01220762203951b88ef484b58bb481a8513
LiangXia <liangxia@live.com>
2023-06-25 22:05:54 +0800

    license

--- LICENSE.md
+++ LICENSE.md
@@ -1,14 +1,13 @@
-## 本项目使用的是 Apache License v2 协议。
+### 项目协议：GNU Affero General Public License。
 
-详情访问：https://www.apache.org/licenses/LICENSE-2.0.html
+详情访问：https://www.gnu.org/licenses/agpl-3.0.en.html
 
-## 补充协义
+### 补充协义
 
-1. 制作公司内部软件产品及交付物为最终软件产品时，无需进行授权。
-2. 禁止使用本项目制作的任何中间交付物，否则均视为侵权。
+1. 制作的软件产品及交付物为最终软件产品时，无需进行授权。
+2. 禁止使用本项目制作的任何中间交付物，无论是否开源，均视为侵权。
 
-## 有关更多信息，请联系作者。
+### 有关更多信息，请联系作者。
 
-### Email：xialiang@vip.qq.com
-
-### QQ群：796316725
+Email：xialiang@vip.qq.com   
+QQ群：796316725
--- kayura-uasp/kayura-uasp-common/src/main/java/org/kayura/uasp/privilege/UserPrivilegePayload.java
+++ kayura-uasp/kayura-uasp-common/src/main/java/org/kayura/uasp/privilege/UserPrivilegePayload.java
@@ -1,17 +1,14 @@
 /*------------------------------------------------------------------------------
- - Copyright 2023 Kayura ( liangxia@live.com )
+ - 版权所有 (C) 2023 kayura
  -
- - Licensed under the Apache License, Version 2.0 (the "License");
- - you may not use this file except in compliance with the License.
- - You may obtain a copy of the License at
+ - 本程序是一个开源软件，根据 GNU 通用公共许可证 (AGPLv3) 的条款发布。
+ - 您可以按照该许可证的规定重新发布和修改本程序。
  -
- -     http://www.apache.org/licenses/LICENSE-2.0
+ - 有关许可证的详细信息，请参阅 LICENSE.md 文件。
+ - 如果您有任何问题、建议或贡献，请联系版权所有者：<liangxia@live.com>
  -
- - Unless required by applicable law or agreed to in writing, software
- - distributed under the License is distributed on an "AS IS" BASIS,
- - WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- - See the License for the specific language governing permissions and
- - limitations under the License.
+ - 本程序基于无任何明示或暗示的担保提供，包括但不限于适销性和特定用途适用性的担保。
+ - 请参阅 GNU 通用公共许可证以获取详细信息。
  -----------------------------------------------------------------------------*/
 
 package org.kayura.uasp.privilege;
